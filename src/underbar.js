dea0b841e30c323ab21a47bd21bd381d705ca9bf
Joyce Liu <liujoycec@gmail.com>
2015-07-20 14:46:20 -0700

    Completed functions _.filter and _.reject.

--- src/underbar.js
+++ src/underbar.js
@@ -90,13 +90,31 @@
   };
 
   // Return all elements of an array that pass a truth test.
-  _.filter = function(collection, test) {
+  _.filter = function(collection, test) { // all tests passed!
+
+    // Write code here:
+    var result = [];
+
+    _.each(collection, function(item) {
+      if (test(item)) {
+        result.push(item);
+      }
+    })
+
+    return result;
+    // end
+
   };
 
   // Return all elements of an array that don't pass a truth test.
-  _.reject = function(collection, test) {
+  _.reject = function(collection, test) { // all tests passed!
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+
+    // Write code here:
+    return _.filter(collection, function(item) {return !test(item)});
+    // end
+
   };
 
   // Produce a duplicate-free version of the array.
