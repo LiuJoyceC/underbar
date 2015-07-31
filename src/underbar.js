54231223894f890dd03238c97882dcc0c49c8a07
Joyce Liu <liujoycec@gmail.com>
2015-07-30 17:15:22 -0700

    Completed _.invoke. Passed all tests.

--- src/underbar.js
+++ src/underbar.js
@@ -422,6 +422,17 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    // Write code here:
+    args = args || [];
+    var result = [];
+    var isFunction = typeof functionOrKey === 'function';
+
+    _.each(collection, function(val) {
+      var newVal = isFunction ? functionOrKey.apply(val,args) : val[functionOrKey](args);
+      result.push(newVal);
+    });
+    return result;
+    // end
   };
 
   // Sort the object's values by a criterion produced by an iterator.
